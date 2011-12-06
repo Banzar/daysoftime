44afc548e7f03b93ffaa797242d3685d1d9faee3
David Ledford <dcledford@ymail.com>
2011-12-06 16:33:35 -0500

    Update daysoftime.rb

--- daysoftime.rb
+++ daysoftime.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'date'
 
   leapz = ["1600", "1604", "1608", "1612", "1616", "1620", "1624",
  "1628", "1632", "1636", "1640", "1644", "1648", "1652", "1656", "1660",
@@ -21,103 +22,192 @@
  "2260", "2264", "2268", "2272", "2276", "2280", "2284", "2288", "2292",
  "2296", "2304", "2308", "2312", "2316", "2320", "2324", "2328", "2332",
  "2336", "2340", "2344", "2348", "2352", "2356", "2360", "2364", "2368",
+
  "2372", "2376", "2380", "2384", "2388", "2392", "2396", "2400"]
  
- dayofchoice = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+ dayofchoice = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"
 "Friday", "Saturday"]
- 
+
  monthname = [nil, "Janurary", "Feburary", "March", "April", "May", 
+
  "June", "July", "August", "September", "October", "November", "December"]
- 
+
 monthdayz = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 monthdayzleapyear = [nil, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 begin # Getting the year
+
 	print "Enter a year (Must be 4 digits): "
+
 	yearz = gets.chomp
+
 	if (yearz.to_i == 0) # Must be a number
+
 		puts "Please use numbers only."
+
 	end
+
 	if (yearz.length != 4) #Making sure the answer is 4 characters
+
 		puts "Please use 4 digits for the year."
+
 	end
+
 	if (yearz.to_i == Time.now.year - 1) # Does not allow a year that has not finished yet
+
 			puts "Please use a year that is before #{Time.now.year - 1}."
+
 	end
+
 	if (yearz.to_i > (Time.now.year - 1))
+
 		puts "Year must be before #{Time.now.year - 1}." 
+
 	end
+
 end until (yearz.to_i < (Time.now.year - 1) and yearz.length == 4 and yearz.to_i != 0)
+
 begin #Gettng the month
+
 	if (yearz.to_i < Time.now.year)
+
 		print "Enter a month (2 digits): "
+
 		monthz = gets.chomp
+
 		if (monthz.to_i == 0)
+
 		puts "Please use numbers only."
+
 		end
+
 		if (monthz.length != 2) #Making sure the answer is 2 characters
+
 			puts "Please use 2 digits for the month."
+
 		end
+
 		if (monthz.to_i > 12) #Making sure the answer is 2 characters
+
 			puts "Please use a number less then or equal to 12."
+
 		end
+
 	end	
 
+
+
 end until (monthz.length == 2 and monthz.to_i != 0 and monthz.to_i <= 12)
+
 if (leapz.include?(yearz))
+
 begin #Getting the day
+
 	print "Enter a day (2 digits): "
+
 	dayz = gets.chomp
+
 	if (dayz.to_i == 0)
+
 		puts "Please use numbers only."
+
 	end
+
 	if (dayz.length != 2) #Making sure the answer is 2 characters
+
 		puts "Please use 2 digits for the day."
+
 	end
+
 	if(dayz.to_i > monthdayzleapyear[monthz.to_i])
+
 		puts "There are only #{monthdayzleapyear[monthz.to_i]} in #{monthname[monthz.to_i]}."
+
 	end
+
 end until (dayz.length == 2 and dayz.to_i != 0 and dayz.to_i <= monthdayz[monthz.to_i])
+
 else
+
 begin #Getting the day
+
 	print "Enter a day (2 digits): "
+
 	dayz = gets.chomp
+
 	if (dayz.to_i == 0)
+
 		puts "Please use numbers only."
+
 	end
+
 	if (dayz.length != 2) #Making sure the answer is 2 characters
+
 		puts "Please use 2 digits for the day."
+
 	end
+
 	if(dayz.to_i > monthdayz[monthz.to_i])
+
 		puts "There are only #{monthdayz[monthz.to_i]} in #{monthname[monthz.to_i]}."
+
 	end
+
 end until (dayz.length == 2 and dayz.to_i != 0 and dayz.to_i <= monthdayz[monthz.to_i])
+
 end
+
 	if leapz.include?(yearz)
+
 		yday = Time.new(yearz, monthz, dayz).yday
+
 		
+
 		daycount = 366 - yday
+
 		yearz = yearz.to_i + 1
+
 	else
-		ydaypast = Time.new(yearz, monthz, dayz).yday
+
+		ydaypast = Date.new(yearz.to_i, monthz.to_i, dayz.to_i).yday
+
 		daycount = 365 - ydaypast
+
 		yearz = yearz.to_i + 1
+
 	end
+
 yearz2 = yearz.to_i
+
 daycount2 = daycount.to_i
+
 		
+
 begin
+
 	if leapz.include?(yearz2.to_s)
+
 		yearz2 = 1 + yearz2
+
 		daycount2 += 366
+
 	else
+
 		yearz2 = 1 + yearz2
+
 		daycount2 += 365
+
 	end
+
 end until (yearz2.to_i == Time.now.year)
+
 yearz = yearz.to_i - 1
+
 ydaynow = Time.now.yday
+
 daycount3 = ydaynow + daycount2
-cday = Time.new(yearz.to_i, monthz.to_i, dayz.to_i).wday
+
+cday = Date.new(yearz.to_i, monthz.to_i, dayz.to_i).wday
+
 puts "#{monthz}/#{dayz}/#{yearz} was a #{dayofchoice[cday]} and was #{daycount3} days ago."
